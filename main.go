36bf0156bdb3c09bfd2af3e4770b6d699f09ab81
Faisal Alghurayri <fghurayri@gmail.com>
2022-01-23 18:28:05 +0300

    add simple web server

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("HEY!")
+	p := getEnv("PORT", "3000")
+	fmt.Println("Started listening on port :" + p)
+	log.Fatalln(http.ListenAndServe(":"+p, nil))
+}
+
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
 }
