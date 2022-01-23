b41a6e7443d68afd06ae2766118b433b5fdf9418
Faisal Alghurayri <fghurayri@gmail.com>
2022-01-23 18:32:18 +0300

    add hello world handler

--- main.go
+++ main.go
@@ -10,7 +10,12 @@ import (
 func main() {
 	p := getEnv("PORT", "3000")
 	fmt.Println("Started listening on port :" + p)
+	http.HandleFunc("/", HandleFunc)
 	log.Fatalln(http.ListenAndServe(":"+p, nil))
+}
+
+func HandleFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World")
 }
 
 func getEnv(key, fallback string) string {
