96cc4c2fa3689e9952cfe31976627fde2fc99d35
Faisal Alghurayri <fghurayri@gmail.com>
2022-01-25 17:21:58 +0300

    broken state: import cycle not allowed thanks to db module and user module

--- lib/models/user.go
+++ lib/models/user.go
@@ -1,10 +1,27 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"go-remix-jokes/lib/db"
+
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
 	Username     string
 	PasswordHash string
 	Jokes        []Joke
+}
+
+func (u *User) CreateUser(username, password string) error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	return db.DB.Create(&User{
+		Username:     username,
+		PasswordHash: string(hashedPassword),
+	}).Error
 }
--- main.go
+++ main.go
@@ -1,17 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
 	"go-remix-jokes/lib/db"
 	"go-remix-jokes/lib/env"
+	"go-remix-jokes/lib/models"
 	"go-remix-jokes/lib/page"
 )
 
 var (
 	homePage       = page.NewPage("pages/index.go.html")
 	jokesIndexPage = page.NewPage("pages/jokes/index.go.html")
+	loginPage      = page.NewPage("pages/auth/login.go.html")
+	registerPage   = page.NewPage("pages/auth/register.go.html")
 )
 
 func main() {
@@ -20,6 +24,9 @@ func main() {
 
 	log.Println("Setting up routes...")
 	http.HandleFunc("/", Index)
+	http.HandleFunc("/auth/login", Login)
+	http.HandleFunc("/auth/register", Register)
+	http.HandleFunc("/auth/logout", Logout)
 	http.HandleFunc("/jokes", JokesIndex)
 
 	p := env.GetEnv("PORT", "3000")
@@ -29,6 +36,54 @@ func main() {
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	homePage.Render(w, "Faisal")
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		loginPage.Render(w, nil)
+
+	case http.MethodPost:
+		r.ParseForm()
+		log.Println(r.PostForm)
+		loginPage.Render(w, nil)
+
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Not found :("))
+	}
+}
+
+func Register(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		registerPage.Render(w, nil)
+
+	case http.MethodPost:
+		err := r.ParseForm()
+		if err != nil {
+			panic(err.Error())
+		}
+
+		email := r.FormValue("email")
+		password := r.FormValue("password")
+
+		u := &models.User{}
+		err = u.CreateUser(email, password)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		http.Redirect(w, r, "/jokes", http.StatusFound)
+
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Not found :("))
+	}
+}
+
+func Logout(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "WIP")
 }
 
 func JokesIndex(w http.ResponseWriter, r *http.Request) {
